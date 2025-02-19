3584c6d1587ff5854405c4313c95663b2aa37c0c
Oliver Hiltz-Perron <olivert831@gmail.com>
2025-02-19 14:54:09 -0500

    pushing to make into proper webpage

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -3,7 +3,7 @@ const Resume = () => {
     <section className="resume">
       <h2>Resume</h2>
       <div className="resume-content">
-        <p>You can download my resume <a href="/OliverResume.pdf" target="_blank">here</a>.</p>
+        <p>You can download my resume <a href={process.env.PUBLIC_URL + "/OliverResume.pdf"} target="_blank">here</a>.</p>
         {/* Add your resume sections here */}
       </div>
     </section>
